7710ccc3af33c85d787a5f72191cf4ea22cef2ab
mahdy-nasr <mahdy.m.nasr@gmail.com>
2018-08-02 14:53:14 +0300

    GPS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { Geolocation } from '@ionic-native/geolocation';
+
 
 @NgModule({
   declarations: [
@@ -34,6 +37,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
+    BarcodeScanner,
+
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,55 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { Geolocation } from '@ionic-native/geolocation';
+
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+	public encodeText: string='mhady';
+	public encodedData: any={};
+	public scannedData: any={};
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private barcodeScanner: BarcodeScanner, private geolocation: Geolocation) {
 
   }
 
+  scanQr() {
+	  	
+	  	this.barcodeScanner.scan().then(barcodeData => {
+	  		this.scannedData = barcodeData;
+	  	}).catch(err => {
+	  		console.log('Error', err);
+	  	});
+  }
+
+  encodeQr() {
+  	
+  	this.geolocation.getCurrentPosition().then((resp) => {
+  		this.encodeText = resp.coords.latitude + ' ' + resp.coords.longitude;
+ // resp.coords.latitude
+ // resp.coords.longitude
+}).catch((error) => {
+  console.log('Error getting location', error);
+});
+
+ /*
+let watch = this.geolocation.watchPosition();
+watch.subscribe((data) => {
+ // data can be a set of coordinates, or an error (if an error occurred).
+ // data.coords.latitude
+ // data.coords.longitude
+});*/
+/*
+const subscription = this.geolocation.watchPosition() //Filter Out Errors
+                              .subscribe(position => {
+this.encodeText = position.coords.latitude + ' ' + position.coords.longitude;});
+
+*/
+
+
+}
 }
